8f66be3c32c85436718753dc6c2fe9629a62e8ee
Dante Dam <laggycomputer@yahoo.com>
2024-10-21 15:19:26 -0700

    move iter stuff to new file

---
+++ popgen/src/iter.rs
@@ -0,0 +1,51 @@
+use crate::{AlleleCounts, Count};
+
+pub struct AlleleCountsSiteIter<'counts> {
+    pub(crate) inner: &'counts AlleleCounts,
+    // index of next for forward iter, index of next for reverse iter
+    pub(crate) next_site_ind: (usize, usize),
+}
+
+impl<'inner> Iterator for AlleleCountsSiteIter<'inner> {
+    type Item = &'inner [Count];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.next_site_ind.0 > self.next_site_ind.1 {
+            return None;
+        }
+
+        match self.inner.count_starts.get(self.next_site_ind.0) {
+            None => None,
+            Some(index) => {
+                let ret = &self.inner.counts[*index..
+                    *self.inner.count_starts
+                        .get(self.next_site_ind.0 + 1)
+                        .unwrap_or(&(self.inner.counts.len() - 1))];
+
+                self.next_site_ind.0 += 1;
+                Some(ret)
+            }
+        }
+    }
+}
+
+impl DoubleEndedIterator for AlleleCountsSiteIter<'_> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.next_site_ind.1 < self.next_site_ind.0 {
+            return None;
+        }
+
+        match self.inner.count_starts.get(self.next_site_ind.1) {
+            None => None,
+            Some(index) => {
+                let ret = &self.inner.counts[*index..
+                    *self.inner.count_starts
+                        .get(self.next_site_ind.1 + 1)
+                        .unwrap_or(&(self.inner.counts.len() - 1))];
+
+                self.next_site_ind.1 -= 1;
+                Some(ret)
+            }
+        }
+    }
+}
