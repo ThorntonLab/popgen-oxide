63dc6808554004cc4918ee4e8374a9895adb906b
Dante Dam <laggycomputer@yahoo.com>
2024-10-31 14:56:59 -0700

    Pi for one site does not exist

--- popgen/src/stats.rs
+++ popgen/src/stats.rs
@@ -26,40 +26,6 @@ pub trait GlobalStatistic {
     fn as_raw(&self) -> f64;
 }
 
-/// The chance pi that two uniformly randomly chosen genotypes at a site are different.
-/// Expect a value in the 1e-3 range.
-/// The complement of this probability is the site homozygosity.
-///
-/// This is known roughly as site heterozygosity or site diversity.
-#[derive(Debug, Copy, Clone)]
-#[repr(transparent)]
-pub struct Pi(f64);
-
-impl Sum for Pi {
-    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
-        Self(iter.map(|x| x.0).sum())
-    }
-}
-
-impl SiteStatistic for Pi {
-    fn from_site(site: SiteCounts) -> Self {
-        // technically should divide both by two here and below but it cancels out
-        let num_pairs = {
-            let count: i64 = site.counts.iter().sum();
-            count * (count - 1)
-        };
-
-        // the number of pairs where the two samples are homozygous, summed over every genotype
-        let num_homozygous_pairs: Count = site.counts.iter().map(|count| count * (count - 1)).sum();
-
-        Self(1f64 - (num_homozygous_pairs as f64 / num_pairs as f64))
-    }
-
-    fn as_raw(&self) -> f64 {
-        self.0
-    }
-}
-
 /// The expected number of differences between two samples over all sites, the "expected pairwise diversity".
 ///
 /// This is the sum of [`Pi`] over all sites.
