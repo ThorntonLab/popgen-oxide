b3184a6cd0667d8bb0800725a9118a89db066873
Kevin Thornton <krthornt@uci.edu>
2025-11-17 08:06:37 -0800

    more infrastructure

--- src/naivecalculations.rs
+++ src/naivecalculations.rs
@@ -1,10 +1,15 @@
 use crate::testdata::GenotypeData;
 use crate::testdata::Site;
+use crate::MultiSiteCounts;
 
 pub fn pi_site(genotypes: &dyn Iterator<Item = GenotypeData>) -> f64 {
     todo!()
 }
 
 pub fn pi<'s>(sites: &'s dyn Iterator<Item = &'s Site>) -> f64 {
+    todo!()
+}
+
+pub fn single_pop_counts<'s>(sites: &'s dyn Iterator<Item = &'s Site>) -> MultiSiteCounts {
     todo!()
 }
