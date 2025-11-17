3c27e5707e868d33aab3ab427f285a3dc97c36c7
Kevin Thornton <krthornt@uci.edu>
2025-11-17 12:26:18 -0800

    we are able to recrate the right answer internally...

--- src/naivecalculations.rs
+++ src/naivecalculations.rs
@@ -5,18 +5,31 @@ pub fn pi_site(genotypes: &mut dyn Iterator<Item = GenotypeData>) -> f64 {
     let mut num_differences = 0_i64;
     let mut num_comparisons = 0_i64;
     let g = genotypes.collect::<Vec<_>>();
+    let num_alleles = g[0].iter().count();
+    let mut temp = vec![0; num_alleles];
     for (i, j) in g.iter().enumerate() {
+        for (x, y) in j.iter().enumerate() {
+            temp[x] += y;
+        }
         for k in g.iter().skip(i) {
             for b in j.iter() {
                 for c in k.iter() {
                     num_differences += (b - c).abs();
-                    num_comparisons += 1;
+                    num_comparisons += b + c;
                 }
             }
         }
     }
+    let sum: i64 = temp.iter().sum();
+    let mut ttl = 0.0;
+    for &i in &temp {
+        let x = i as f64;
+        let n = sum as f64;
+        ttl += (x/n)*((x-1.)/(n-1.));
+    }
+    ttl = 1. - ttl;
     println!(
-        "{num_differences}, {num_comparisons} {}",
+        "{temp:?} {ttl} {num_differences}, {num_comparisons} {}",
         (num_differences as f64 / num_comparisons as f64)
     );
     num_differences as f64 / num_comparisons as f64
