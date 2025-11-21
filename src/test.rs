f66a42613d1fd937e77b737ec8f97d936790664a
Kevin Thornton <krthornt@uci.edu>
2025-11-21 12:44:57 -0800

    loop over ploidy in 1st test

--- src/test.rs
+++ src/test.rs
@@ -463,27 +463,29 @@ chr0	1	.	G	A	.	.	.	GT	/0	/1	/1	/0	/1	/1	/0	/0	/.	/.	/0	/0	/1	/1	/1	/1	/0	/."#
         use rand::prelude::*;
 
         let mut rng = StdRng::seed_from_u64(54321);
-        let freqs = [0.25, 0.5, 0.25]; // fixed allele freqs per site
-        let mut sites = vec![];
-        // make 10 random sites.
-        // No missing data, etc..
-        for _ in 0..10 {
-            let site = crate::testdata::random_site_rng(10, 2, &freqs, None, &mut rng);
-            sites.push(site);
-        }
-        // convert to our normal format
-        let counts = crate::testdata::single_pop_counts(&mut sites.iter());
-        // get the calcs
-        let pi_from_counts = GlobalPi::from_iter_sites(counts.iter());
-        let pi_naive = crate::naivecalculations::pi(&mut sites.iter_mut());
-        // compare
-        if pi_naive.is_nan() {
-            assert!(pi_from_counts.as_raw().is_nan());
-        } else {
-            assert!(
-                (pi_from_counts.as_raw() - pi_naive).abs() <= 1e-10,
-                "{pi_from_counts:?} != {pi_naive}"
-            );
+        for ploidy in [1, 2, 3, 4] {
+            let freqs = [0.25, 0.5, 0.25]; // fixed allele freqs per site
+            let mut sites = vec![];
+            // make 10 random sites.
+            // No missing data, etc..
+            for _ in 0..10 {
+                let site = crate::testdata::random_site_rng(10, ploidy, &freqs, None, &mut rng);
+                sites.push(site);
+            }
+            // convert to our normal format
+            let counts = crate::testdata::single_pop_counts(&mut sites.iter());
+            // get the calcs
+            let pi_from_counts = GlobalPi::from_iter_sites(counts.iter());
+            let pi_naive = crate::naivecalculations::pi(&mut sites.iter_mut());
+            // compare
+            if pi_naive.is_nan() {
+                assert!(pi_from_counts.as_raw().is_nan());
+            } else {
+                assert!(
+                    (pi_from_counts.as_raw() - pi_naive).abs() <= 1e-10,
+                    "{pi_from_counts:?} != {pi_naive}"
+                );
+            }
         }
     }
 
