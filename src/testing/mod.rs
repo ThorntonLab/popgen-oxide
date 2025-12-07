9d0087d0041c77c0b29c07af4f871522d248348f
Kevin Thornton <krthornt@uci.edu>
2025-12-07 09:01:18 -0800

    move watterson and noodles tests, add files this time...

--- src/testing/mod.rs
+++ src/testing/mod.rs
@@ -2,5 +2,14 @@ mod naivecalculations;
 mod test;
 mod testdata;
 
+// tests of CALCULATIONS go below
+
 mod pi;
 mod wattersons_theta;
+
+// tests of OTHER THINGS, like
+// input formats, follow
+
+#[cfg(feature = "noodles")]
+mod noodles_vcf;
+
