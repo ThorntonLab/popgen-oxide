390649b7ac0b7c663c43b06fec5b2ed3223f8677
Dante Dam <laggycomputer@yahoo.com>
2026-02-12 08:40:09 -0800

    test: naive F_ST (#30)

--- src/testing/fst.rs
+++ src/testing/fst.rs
@@ -1,7 +1,10 @@
 use crate::stats::FStatisticParts;
 use crate::stats::GlobalPi;
 use crate::stats::GlobalStatistic;
+use crate::testing::testdata::RandomSiteOptions;
 use crate::MultiPopulationCounts;
+use std::borrow::Cow;
+use std::collections::HashMap;
 
 #[test]
 fn f_st_empty() {
@@ -84,4 +87,74 @@ fn f_st() {
         .abs()
             < f64::EPSILON
     );
+}
+
+#[test]
+fn f_st_from_random_data() {
+    use rand::prelude::*;
+    let n_sites = 5;
+    let pop_weights = [0.1, 0.2, 0.3, 0.4];
+    let n_alleles = 3;
+
+    let mut rng = StdRng::seed_from_u64(54321);
+    for n_pops in 2..4 {
+        for ploidy in 1..3 {
+            let pops = (0..n_pops)
+                .map(|_| {
+                    let mut sites = vec![];
+                    for _ in 0..n_sites {
+                        let site = crate::testing::testdata::random_site_rng(
+                            5,
+                            ploidy,
+                            &[0.25, 0.5, 0.25],
+                            Some(RandomSiteOptions {
+                                missing_data_rate: Some(0.1),
+                            }),
+                            &mut rng,
+                        );
+                        sites.push(site);
+                    }
+                    sites
+                })
+                .collect::<Vec<_>>();
+
+            let mut counts = MultiPopulationCounts::of_empty_populations(n_pops);
+            #[expect(
+                clippy::needless_range_loop,
+                reason = "https://github.com/rust-lang/rust-clippy/issues/16344"
+            )]
+            for s in 0..n_sites {
+                counts
+                    .extend_populations_from_site(|pop_i| {
+                        let alleles = pops[pop_i][s]
+                            .iter()
+                            .flat_map(|gts| gts.iter())
+                            .collect::<Vec<_>>();
+                        let mut counts = HashMap::new();
+                        for non_missing_allele_id in alleles.iter().flatten() {
+                            *counts.entry(non_missing_allele_id).or_default() += 1;
+                        }
+
+                        let counts_vec = (0..n_alleles)
+                            .map(|allele_id| counts.get(&allele_id).cloned().unwrap_or_default())
+                            .collect::<Vec<_>>();
+                        (Cow::Owned(counts_vec), alleles.len())
+                    })
+                    .unwrap();
+            }
+
+            let f_st_from_counts = counts.f_st_if(|i| Some(pop_weights[i]));
+            let (pi_total_naive, pi_self_naive, pi_between_naive) =
+                crate::testing::naivecalculations::f_st(
+                    &mut pops
+                        .iter()
+                        .cloned()
+                        .zip(pop_weights.iter())
+                        .map(|(p, &w)| (w, p)),
+                );
+            assert!((1.0 - (f_st_from_counts.pi_T() / pi_total_naive)).abs() < 0.00001);
+            assert!((1.0 - (f_st_from_counts.pi_S().unwrap() / pi_self_naive)).abs() < 0.00001);
+            assert!((1.0 - (f_st_from_counts.pi_B().unwrap() / pi_between_naive)).abs() < 0.00001);
+        }
+    }
 }
--- src/testing/naivecalculations.rs
+++ src/testing/naivecalculations.rs
@@ -65,3 +65,103 @@ pub fn watterson_theta<'s>(sites: &'s mut dyn Iterator<Item = &'s mut Site>) -> 
     }
     rv
 }
+
+pub fn pi_ij(pop1: &mut dyn Iterator<Item = Site>, pop2: &mut dyn Iterator<Item = Site>) -> f64 {
+    let mut accum = 0f64;
+
+    for (site1, site2) in pop1.zip(pop2) {
+        let mut homozygous = 0u64;
+        let mut num_comparisons = 0u64;
+        for gt1 in site1.iter().flat_map(|gt| gt.iter()) {
+            for gt2 in site2.iter().flat_map(|gt| gt.iter()) {
+                if let Some((l, r)) = gt1.zip(gt2) {
+                    if l == r {
+                        homozygous += 1;
+                    }
+                    num_comparisons += 1;
+                }
+            }
+        }
+        accum += 1.0 - (homozygous as f64 / num_comparisons as f64);
+    }
+
+    accum
+}
+
+// return pi_T, pi_S, pi_B
+pub fn f_st<Sites>(populations: &mut dyn Iterator<Item = (f64, Sites)>) -> (f64, f64, f64)
+where
+    Sites: IntoIterator<Item = Site>,
+{
+    let pops_and_weights: Vec<(f64, Vec<Site>)> = populations
+        .map(|(w, pop)| (w, pop.into_iter().collect()))
+        .collect();
+    let weights = pops_and_weights
+        .iter()
+        .map(|(w, _pop)| w)
+        .copied()
+        .collect::<Vec<_>>();
+    let mut populations = pops_and_weights
+        .into_iter()
+        .map(|(_w, pop)| pop)
+        .collect::<Vec<_>>();
+
+    let pi_ii = populations
+        .iter_mut()
+        .map(|pop| pi(&mut pop.iter_mut()))
+        .collect::<Vec<_>>();
+
+    let mut pi_ij_computed = vec![vec![None; populations.len()]; populations.len()];
+    for i in 0..populations.len() {
+        for j in 0..i {
+            pi_ij_computed[i][j] = Some(pi_ij(
+                &mut populations[i].iter().cloned(),
+                &mut populations[j].iter().cloned(),
+            ));
+            pi_ij_computed[j][i] = pi_ij_computed[i][j];
+        }
+    }
+
+    // equation 1a
+    let pi_total = {
+        let pi_ii_term = (0..populations.len())
+            .map(|i| weights[i] * weights[i] * pi_ii[i])
+            .sum::<f64>();
+        let pi_ij_term = {
+            let mut tot = 0f64;
+            for i in 0..populations.len() {
+                for j in 0..i {
+                    tot += weights[i] * weights[j] * pi_ij_computed[i][j].unwrap();
+                }
+            }
+            2.0 * tot
+        };
+
+        pi_ii_term + pi_ij_term
+    };
+
+    // equation 1b
+    let pi_self = ((0..populations.len())
+        .map(|i| weights[i] * weights[i] * pi_ii[i])
+        .sum::<f64>())
+        / (0..populations.len())
+            .map(|i| weights[i] * weights[i])
+            .sum::<f64>();
+
+    // equation 1c
+    let pi_between = {
+        let mut num = 0f64;
+        let mut denom = 0f64;
+
+        for i in 0..populations.len() {
+            for j in 0..i {
+                num += weights[i] * weights[j] * pi_ij_computed[i][j].unwrap();
+                denom += weights[i] * weights[j];
+            }
+        }
+
+        num / denom
+    };
+
+    (pi_total, pi_self, pi_between)
+}
