7052e31284e5155cdd20ec8e63d108faaaeb3794
Dante Dam <d@dantedam.com>
2026-05-22 19:01:48 -0700

    this too

--- examples/allel_.py
+++
@@ -1,6 +0,0 @@
-import allel
-
-vcf = allel.read_vcf("ALL.chr18.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz")
-gt = allel.GenotypeArray(vcf['calldata/GT'])
-ac = gt.count_alleles()
-print(allel.mean_pairwise_difference(ac).sum())
