b151604861346b7a38fbad76a91b5117e93dc7ec
Dante Dam <d@dantedam.com>
2026-09-16 14:22:25 -0700

    darn imports

--- src/iter.rs
+++ src/iter.rs
@@ -1,5 +1,7 @@
-use crate::{Count, SingleSampleAlleleCounts};
 use crate::{AlleleCounts, SampleAlleleCounts};
+use crate::SingleSampleAlleleCounts;
+#[cfg(test)]
+use crate::Count;
 
 pub struct SampleAlleleCountsPopulationIter<'inner> {
     pub(crate) inner: &'inner SampleAlleleCounts,
