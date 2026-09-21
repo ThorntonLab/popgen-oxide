bc8c56119db15e1f4cfaebf00205a11d27af3ead
Kevin R. Thornton <krthornt@uci.edu>
2026-09-21 13:22:33 -0700

    issue w/equal sample sizes, too

--- python/integration-tests/tests/test_fstatistics.py
+++ python/integration-tests/tests/test_fstatistics.py
@@ -46,6 +46,47 @@ def test_f2_all_sample_nodes_infinite_sites_mutation(anc_seed, mut_seed):
     assert np.isclose(f2, f2_py, 1e-10)
 
 
+@given(anc_seed=integers(1, 42000000), mut_seed=integers(1, 42000000), num_samples=integers(8, 100))
+def test_f2_all_sample_nodes_infinite_sites_mutation_vary_sample_size(anc_seed, mut_seed, num_samples):
+    yaml = """
+    time_units: generations
+    demes:
+     - name: ancestor
+       epochs:
+        - start_size: 10000
+          end_time: 500
+     - name: d0
+       ancestors: [ancestor]
+       epochs:
+        - start_size: 10000
+     - name: d1
+       ancestors: [ancestor]
+       epochs:
+        - start_size: 10000
+    """
+    graph = demes.loads(yaml)
+    demog = msprime.Demography.from_demes(graph)
+    random.seed(mut_seed)
+    ts = msprime.sim_ancestry([msprime.SampleSet(100, population=1), msprime.SampleSet(100, population=2)], sequence_length=1000000, demography=demog,
+                              recombination_rate=1.2e-8, random_seed=anc_seed)
+    ts = msprime.sim_mutations(
+        ts, rate=1.2e-8, random_seed=mut_seed, model=msprime.InfiniteSites())
+    tsholder = integration_tests.ts_holder_from_tables(ts.tables.copy())
+    samples = [
+        [i for i in ts.samples(population=1)],
+        [i for i in ts.samples(population=2)],
+    ]
+    random.shuffle(samples[0])
+    random.shuffle(samples[1])
+    subsamples = [samples[0][:num_samples],
+                  samples[1][:num_samples]]
+    counts = integration_tests.counts_from_ts_holder_multi_sample_sets(
+        tsholder, subsamples)
+    f2 = integration_tests.f2(counts, 0, 1)
+    f2_py = ts.f2(sample_sets=subsamples, span_normalise=False)
+    assert np.isclose(f2, f2_py, 1e-10), f"{f2} !~= {f2_py}"
+
+
 @given(anc_seed=integers(1, 42000000), mut_seed=integers(1, 42000000),
        # NOTE: we get failures with smaller sample sizes, which we have to investigate later
        num_sample_nodes_deme1=integers(8, 100),
