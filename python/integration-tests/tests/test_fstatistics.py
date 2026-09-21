db9c702c8b76349c1ff9c963b5783e57c71c88d4
Kevin R. Thornton <krthornt@uci.edu>
2026-09-21 12:32:54 -0700

    now it runs

--- python/integration-tests/tests/test_fstatistics.py
+++ python/integration-tests/tests/test_fstatistics.py
@@ -28,12 +28,14 @@ def test_f2_all_sample_nodes(anc_seed, mut_seed):
     """
     graph = demes.loads(yaml)
     demog = msprime.Demography.from_demes(graph)
-    ts = msprime.sim_ancestry([0, 100, 100], sequence_length=1000000, demography=demog,
+    ts = msprime.sim_ancestry([msprime.SampleSet(100, population=1), msprime.SampleSet(100, population=2)], sequence_length=1000000, demography=demog,
                               recombination_rate=1.2e-8, random_seed=anc_seed)
+    ts = msprime.sim_mutations(
+        ts, rate=1.2e-8, random_seed=mut_seed, model=msprime.InfiniteSites())
     tsholder = integration_tests.ts_holder_from_tables(ts.tables.copy())
     counts = integration_tests.counts_from_ts_holder_multi_sample_sets(
         tsholder,
         [
-            [i for i in range(ts.num_nodes) if ts.node(i).deme == 1],
-            [i for i in range(ts.num_nodes) if ts.node(i).deme == 2],
+            [i for i in range(ts.num_nodes) if ts.node(i).population == 1],
+            [i for i in range(ts.num_nodes) if ts.node(i).population == 2],
         ])
